4e0434246f43a9366d902365f74708d86541d10e
seethersan <carlos_jcez@hotmail.com>
2016-04-07 15:43:00 -0500

    corrigiendo errores ortograficos

--- ple/ple_peru/utils.py
+++ ple/ple_peru/utils.py
@@ -46,7 +46,7 @@ def to_txt(data, tipo, nombre, primer=None):
 				row['resumen_diario']+'|'+
 				row['tipo_documento']+'|'+
 				row['numero_documento']+'|'+
-				unicode(row['nombre_proveedor'], 'utf-8').encode('utf-8')+'|'+
+				unicode(row['nombre_proveedor'],'utf-8').decode('utf-8')+'|'+
 				str(row['base_imponible'])+'|'+
 				str(row['monto_impuesto'])+'|'+
 				row['base_imponible_exportacion']+'|'+
@@ -89,7 +89,7 @@ def to_txt(data, tipo, nombre, primer=None):
 				row['resumen_diario']+"|"+
 				row['tipo_documento']+"|"+
 				row['numero_documento']+"|"+
-				row['nombre_cliente'].encode('utf-8')+"|"+
+				unicode(row['nombre_cliente'],'utf-8').decode('utf-8')+"|"+
 				row['valor_exportacion']+"|"+
 				str(row['base_imponible'])+"|"+
 				row['descuento']+"|"+
@@ -211,7 +211,7 @@ def to_csv(data, tipo, nombre, primer=None):
 				row['resumen_diario']+','+
 				row['tipo_documento']+','+
 				row['numero_documento']+','+
-				unicode(row['nombre_proveedor'], 'utf-8').encode('utf-8')+','+
+				unicode(row['nombre_proveedor'],'utf-8').decode('utf-8')+','+
 				str(row['base_imponible'])+','+
 				str(row['monto_impuesto'])+','+
 				row['base_imponible_exportacion']+','+
@@ -290,7 +290,7 @@ def to_csv(data, tipo, nombre, primer=None):
 				row['resumen_diario']+","+
 				row['tipo_documento']+","+
 				row['numero_documento']+","+
-				row['nombre_cliente'].encode('utf-8')+","+
+				unicode(row['nombre_cliente'],'utf-8').decode('utf-8')+","+
 				row['valor_exportacion']+","+
 				str(row['base_imponible'])+","+
 				row['descuento']+","+
