60ccc77e01efff66d956d818819d716cb30b5072
seethersan <carlos_jcez@hotmail.com>
2016-06-29 21:00:14 -0500

    arreglando anotacion libro de compras

--- ple/ple_peru/doctype/libro_electronico_de_compras/libro_electronico_de_compras.py
+++ ple/ple_peru/doctype/libro_electronico_de_compras/libro_electronico_de_compras.py
@@ -99,7 +99,7 @@ def get_purchase_invoices(year, periodo):
 			"" as error_3,
 			"" as error_4,
 			'1' as indicador_pago,
-			IF(CONCAT(DATE_FORMAT(posting_date,'%Y-%m'),'-01')>=bill_date,'6','0') as anotacion
+			'0' as anotacion
 		from
 			`tabPurchase Invoice` purchase_invoice
 		left join
