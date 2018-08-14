938a528e64367cf8b5b5c868d685ebeb69251857
seethersan <carlos_jcez@hotmail.com>
2018-08-14 16:52:24 -0500

    FIX: nombre de ple de ventas vacio

--- ple/ple_peru/doctype/libro_electronico_de_ventas/libro_electronico_de_ventas.py
+++ ple/ple_peru/doctype/libro_electronico_de_ventas/libro_electronico_de_ventas.py
@@ -172,7 +172,7 @@ def export_libro_de_ventas(year, periodo, ruc):
 		codigo_periodo = year + "11"
 	elif periodo=='Diciembre':
 		codigo_periodo = year + "12"
-	nombre = "LE"+str(ruc)+codigo_periodo+'00140100'+'00'+'1'+'1'+'1'+'1'
+	nombre = "LE"+str(ruc)+codigo_periodo+'00140100'+'00'+'1'+ ('1' if data else '0') +'1'+'1'
 	send_txt_to_client(data,nombre, tipo)
 
 
